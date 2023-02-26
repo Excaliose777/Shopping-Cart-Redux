ca2b5071e2dce0cbad252d72a6d854bca8aac6f7
Alaminxab <alaminxab@gmail.com>
2023-02-26 19:16:48 +0000

    added reducers and actions to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Navbar from "./components/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
 import CartContainer from "./components/CartContainer";
+import { useEffect } from "react";
+import Modal from "./components/modal";
 
 function App() {
+  const {cartItems} = useSelector((store) => store.cart)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
   return (
     <main>
+      <Modal/>
       <Navbar/>
       <CartContainer/>
     </main>
